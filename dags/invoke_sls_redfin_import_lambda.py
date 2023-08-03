ca83c22b49373b6de9e64bfc02557529fc60313e
Zach Fisic <zachfisic@gmail.com>
2023-08-02 22:02:54 -0400

    revert interval

--- dags/invoke_sls_redfin_import_lambda.py
+++ dags/invoke_sls_redfin_import_lambda.py
@@ -70,7 +70,7 @@ def delete_messages(ti=None) -> None:
 
 with DAG(
     dag_id='redfin_raw_data_import',
-    schedule_interval='*/5 * * * *',
+    schedule_interval=None,
     start_date=datetime(2023, 7, 20),
     dagrun_timeout=timedelta(minutes=60),
     tags=['example'],
